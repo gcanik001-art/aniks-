bc1c5f940684dcdd225b1ae8ba6b36d82f62cdf7
gcanik001-art <gcanik001@gmail.com>
2026-06-25 08:56:01 +0600

    added

--- anik.c
+++ anik.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 int main(){
-int a;
+int a,sum=0;
 scanf("%d",&a);
 printf("%d",a);
 
 printf("this is anik who is your best");
+
+for(int i=1;i<=10;i++){
+
+sum=sum+i;
+
+
+
+}
 return 0;
 
 }
